e01ed3db4497cab17939a0f6c4d6edc7aa3a50ab
torabekov_08 <diyorbektorabekov08@gmail.com>
2024-03-04 16:48:28 +0500

    commit

--- button.py
+++ button.py
@@ -2,5 +2,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 catalog = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='Uzbek', callback_data='uzbek'),InlineKeyboardButton(text='Turk', callback_data='turk')],
+    [InlineKeyboardButton(text="Rus", callback_data='rus'), InlineKeyboardButton(text="Azarbayjan",callback_data="azar")],
+    [InlineKeyboardButton(text="Kazak", callback_data='kazak')]
 
 ])
--- main.py
+++ main.py
@@ -7,27 +7,50 @@ from aiogram.types import Message, CallbackQuery
 from button import catalog
 from web_uzb import audio
 from web_turk import audio1
+from web_rus import audio2
+from web_azarb import audio3
+from web_kazak import audio4
 
-
-TOKEN = ""
+TOKEN = "7163530590:AAEQ_e86WsCKTgOxPtRdY2Vw81-n0MTFCyk"
 
 dp = Dispatcher()
 
 
 @dp.message(Command("start"))
 async def start(message: Message):
-    await message.answer(f"Salom {message.from_user.full_name}",reply_markup=catalog)
+    await message.answer(f"<b>Hello</b> {message.from_user.full_name} <b>which country music do you need?</b>",reply_markup=catalog,parse_mode="HTML")
 
 
 @dp.callback_query(F.data == "uzbek")
 async def uzb(call:CallbackQuery):
-    for x in audio:
-        await call.message.answer_audio(audio=x)
+    for a in audio:
+        await call.message.answer_audio(audio=a)
 
 @dp.callback_query(F.data == "turk")
 async def uzb(call:CallbackQuery):
-    for x in audio1:
-        await call.message.answer_audio(audio=x)
+    for b in audio1:
+        await call.message.answer_audio(audio=b)
+
+
+@dp.callback_query(F.data == "rus")
+async def uzb(call:CallbackQuery):
+    for d in audio2:
+        await call.message.answer_audio(audio=d)
+
+
+@dp.callback_query(F.data == "azar")
+async def uzb(call:CallbackQuery):
+    for e in audio3:
+        await call.message.answer_audio(audio=e)
+
+
+@dp.callback_query(F.data == "kazak")
+async def uzb(call:CallbackQuery):
+    for f in audio4:
+        await call.message.answer_audio(audio=f)
+
+
+
 
 
 async def main() -> None:
